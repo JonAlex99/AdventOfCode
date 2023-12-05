d64a2688934d2b807a3dd309b70268acf23d4e26
JonAlex99 <jonalexpetursson@gmail.com>
2023-12-05 22:23:12 +0000

    Búinn að finna allt rétt, núna vantar bara að bera saman tölur og stafi og svo að reikna

--- 2023/1des/1des-2.js
+++ 2023/1des/1des-2.js
@@ -3,37 +3,61 @@ import fs from "fs";
 const filePath = "./1des-demo2.txt";
 // const filePath = "./1des-2.txt";
 
-function fixTheNumbers(line){
-    var tempLine = line.toLowerCase();
-    const numberWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    var firstNumbers = {};
-    var lastNumbers = {};
-    var firstNumber;
-    var lastNumber;
+// function fixTheNumbers(line){
+//     var tempLine = line.toLowerCase();
+//     const numberWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+//     var firstNumbers = {};
+//     var lastNumbers = {};
+//     var firstNumber;
+//     var lastNumber;
 
-    if(tempLine.includes("one") || tempLine.includes("1")){
+//     if(tempLine.includes("one") || tempLine.includes("1")){
 
-        tempLine.indexOf("one") < tempLine.indexOf("1") ? firstNumber = tempLine.indexOf("one") : firstNumber = tempLine.indexOf("1");
-        tempLine.lastIndexOf("one") > tempLine.lastIndexOf("1") ? lastNumber = tempLine.lastIndexOf("one") : lastNumber = tempLine.lastIndexOf("1");
+//         tempLine.indexOf("one") < tempLine.indexOf("1") ? firstNumber = tempLine.indexOf("one") : firstNumber = tempLine.indexOf("1");
+//         tempLine.lastIndexOf("one") > tempLine.lastIndexOf("1") ? lastNumber = tempLine.lastIndexOf("one") : lastNumber = tempLine.lastIndexOf("1");
         
-    }
+//     }
     
 
 
-    tempLine = tempLine.replaceAll("one", '1')
-    tempLine = tempLine.replaceAll("two", '2')
-    tempLine = tempLine.replaceAll("three", '3')
-    tempLine = tempLine.replaceAll("four", '4')
-    tempLine = tempLine.replaceAll("five", '5')
-    tempLine = tempLine.replaceAll("six", '6')
-    tempLine = tempLine.replaceAll("seven", '7')
-    tempLine = tempLine.replaceAll("eight", '8')
-    tempLine = tempLine.replaceAll("nine", '9')
-    return tempLine;
-}
+//     tempLine = tempLine.replaceAll("one", '1')
+//     tempLine = tempLine.replaceAll("two", '2')
+//     tempLine = tempLine.replaceAll("three", '3')
+//     tempLine = tempLine.replaceAll("four", '4')
+//     tempLine = tempLine.replaceAll("five", '5')
+//     tempLine = tempLine.replaceAll("six", '6')
+//     tempLine = tempLine.replaceAll("seven", '7')
+//     tempLine = tempLine.replaceAll("eight", '8')
+//     tempLine = tempLine.replaceAll("nine", '9')
+//     return tempLine;
+// }
 
 function findTheIntegers(line){
+    var lowHighDictionary = {};
+    lowHighDictionary['lowest'] = {};
+    lowHighDictionary['highest'] = {};
+    var lowestIndex;
+    var highestIndex;
 
+    for (var i = 0; i < line.length; i++){
+        if (!isNaN(line[i])){
+            lowestIndex = line[i].toString();
+            lowHighDictionary['lowest'][lowestIndex] = i
+            break;
+        }
+    }
+    
+    for (var i = line.length - 1; i >= 0; i--){
+        if (!isNaN(line[i])){
+            highestIndex = line[i].toString();
+            lowHighDictionary['highest'][highestIndex] = i
+            break;
+        }
+    }
+
+    console.log(line)
+    console.log(lowHighDictionary);
+    return lowHighDictionary;
 }
 
 function findTheStrings(line){
@@ -45,9 +69,10 @@ function findTheStrings(line){
     var highestIndex = Number.MIN_VALUE;
     numberWords.forEach((number) => {
         var startIndex = 0;
-
-
+        var tempDict = {};
+        
         while (startIndex < line.length){
+            tempDict = {};
             const index = line.indexOf(number, startIndex);
 
             if (index === -1){
@@ -55,12 +80,14 @@ function findTheStrings(line){
             }
 
             if(lowestIndex > index){
-                lowHighDictionary['lowest'][number] = index;
+                tempDict[number] = index;
+                lowHighDictionary['lowest'] = tempDict;
                 lowestIndex = index;
             }
 
             if(highestIndex < index){
-                lowHighDictionary['highest'][number] = index;
+                tempDict[number] = index;
+                lowHighDictionary['highest']= tempDict;
                 highestIndex = index;
             }
 
@@ -68,7 +95,7 @@ function findTheStrings(line){
         }
     })
 
-    console.log(lowHighDictionary);
+    // console.log(lowHighDictionary);
     return lowHighDictionary;
 }
 
@@ -83,7 +110,8 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     var totalSum = 0;
     lines.forEach((line) => {
         line = line.replace(/(\r\n|\n|\r)/gm, "");
-        line = findTheStrings(line);
+        var stringInLine = findTheStrings(line);
+        var intInLine = findTheIntegers(line);
         var first = 0;
         var last = 0;
         var total = 0;
